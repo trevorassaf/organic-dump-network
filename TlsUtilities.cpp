d27967b90bdc94311e4c19bcd698702d6593253a
Trevor Assaf <bozkurtus@google.com>
2019-12-21 16:54:30 -0800

    Add send/read functions

--- TlsUtilities.cpp
+++ TlsUtilities.cpp
@@ -1,6 +1,17 @@
 #include "TlsUtilities.h"
 
 #include <cassert>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <utility>
+
+#include  <arpa/inet.h>
+
+#include  <openssl/bio.h>
+#include  <openssl/ssl.h>
+#include  <openssl/err.h>
 
 #include <glog/logging.h>
 
@@ -8,6 +19,14 @@
 
 namespace network
 {
+
+void InitOpenSslForProcess()
+{
+    ERR_load_BIO_strings(); 
+    OpenSSL_add_all_algorithms(); 
+    SSL_load_error_strings();
+}
+
 bool SendTlsMessage(TlsConnection *connection, const std::string &message)
 {
     assert(connection);
@@ -96,4 +115,164 @@ bool ReadTlsMessage(
 
     return true;
 }
+
+bool SendTlsData(
+    TlsConnection *cxn,
+    const uint8_t *data,
+    size_t data_len)
+{
+    assert(cxn);
+    assert(data);
+
+    size_t bytes_remaining = data_len;
+    while (bytes_remaining > 0)
+    {
+        size_t bytes_written;
+        bool eof = false;
+        int write_errno;
+        bool write_succeeded = cxn->Write(
+            data,
+            bytes_remaining,
+            &bytes_written,
+            &eof,
+            &write_errno);
+
+        if (!write_succeeded)
+        {
+            if (write_errno != EWOULDBLOCK && write_errno != EAGAIN)
+            {
+                LOG(ERROR) << "Encountered error when writing to TLS connection: "
+                           << strerror(write_errno);
+                return false;
+            }
+        }
+        else if (eof)
+        {
+            return true;
+        }
+
+        bytes_remaining -= bytes_written;
+    }
+
+    return true;
+}
+
+bool ReadTlsData(
+    TlsConnection *cxn,
+    uint8_t *data,
+    size_t data_len)
+{
+    assert(cxn);
+    assert(data);
+
+    size_t bytes_remaining = data_len;
+
+    while (bytes_remaining > 0)
+    {
+        size_t bytes_read;
+        bool eof = false;
+        int read_errno;
+        bool read_succeeded = cxn->Read(
+            data,
+            bytes_remaining,
+            &bytes_read,
+            &eof,
+            &read_errno);
+
+        if (!read_succeeded)
+        {
+            if (read_errno != EWOULDBLOCK && read_errno != EAGAIN)
+            {
+                LOG(ERROR) << "Failed while reading from TLS connection: " << strerror(read_errno);
+                return false;
+            }
+
+            LOG(ERROR) << "bozkurtus -- would block";
+        }
+        else if (eof)
+        {
+            LOG(ERROR) << "TlsUtilities::ReadTlsMessage() -- eof!";
+            return true;
+        }
+
+        bytes_remaining -= bytes_read;
+
+        LOG(ERROR) << "ReadTlsMessage() loop. bytes remaining: " << bytes_remaining
+                   << ". bytes read: " << bytes_read;
+    }
+
+    LOG(ERROR) << "TlsUtilities::ReadTlsMessage() -- end";
+
+    return true;
+}
+
+bool SendTlsProtobufMessage(
+    TlsConnection *cxn,
+    uint8_t msg_type,
+    google::protobuf::Message *msg,
+    bool *out_cxn_closed)
+{
+    assert(cxn);
+    assert(msg);
+
+    /**
+     *  Packet format: [type -- 1 byte] [length -- 4 bytes] [message -- length bytes]
+     */
+
+    if (!SendTlsData(cxn, &msg_type, 1))
+    {
+        LOG(ERROR) << "Failed to send msg type: " << static_cast<int>(msg_type);
+        return false;
+    }
+
+    size_t msg_len = msg->ByteSizeLong();
+    uint32_t msg_len_network_order = htonl(static_cast<uint32_t>(msg_len));
+    if (!SendTlsData(cxn, reinterpret_cast<uint8_t *>(&msg_len_network_order), sizeof(msg_len_network_order)))
+    {
+        LOG(ERROR) << "Failed to send msg length: " << msg_len;
+        return false;
+    }
+
+    std::unique_ptr<uint8_t[]> data_buffer{new uint8_t[msg_len]};
+    msg->InternalSerializeWithCachedSizesToArray(true, data_buffer.get());
+
+    if (!SendTlsData(cxn, data_buffer.get(), msg_len))
+    {
+        LOG(ERROR) << "Failed to send protbuf message body";
+        return false;
+    }
+   
+    return true; 
+}
+
+bool ReadTlsProtobufMessageHeader(
+    TlsConnection *cxn,
+    uint8_t *out_type,
+    uint32_t *out_size)
+{
+    assert(cxn);
+    assert(out_type);
+    assert(out_size);
+
+    /**
+     *  Header format: [type -- 1 byte] [length -- 4 bytes]
+     */
+    assert(sizeof(*out_type) == 1);
+    if (!ReadTlsData(cxn, out_type, sizeof(*out_type)))
+    {
+        LOG(ERROR) << "Failed to read protobuf message type";
+        return false;
+    }
+
+    assert(sizeof(*out_size) == 4);
+    if (!ReadTlsData(cxn, reinterpret_cast<uint8_t *>(out_size), sizeof(*out_size)))
+    {
+        LOG(ERROR) << "Failed to read protobuf message type";
+        return false;
+    }
+    *out_size = ntohl(*out_size);
+
+    return true;
+}
+
 } // namespace network
--- TlsUtilities.h
+++ TlsUtilities.h
@@ -1,19 +1,24 @@
 #ifndef NETWORK_TLSUTILITIES_H
 #define NETWORK_TLSUTILITIES_H
 
+#include <cstdint>
+#include <cstdlib>
+#include <memory>
+#include <string>
+
 #include  <openssl/bio.h>
 #include  <openssl/ssl.h>
 #include  <openssl/err.h>
 
 #include <glog/logging.h>
 
-#include <cstdint>
-#include <cstdlib>
-#include <memory>
-#include <string>
+#include <google/protobuf/message.h>
 
 namespace network
 {
+
+void InitOpenSslForProcess();
+
 struct SslDeleter
 {
 void operator()(SSL *ssl)
@@ -63,6 +68,27 @@ bool ReadTlsMessage(
     uint8_t *read_buffer,
     size_t length,
     std::string *out_message);
+
+bool SendTlsProtobufMessage(
+    TlsConnection *cxn,
+    uint8_t msg_type,
+    google::protobuf::Message *msg,
+    bool *out_cxn_closed);
+
+bool SendTlsData(
+    TlsConnection *cxn,
+    const uint8_t *data,
+    size_t data_len);
+
+bool ReadTlsData(
+    TlsConnection *cxn,
+    uint8_t *data,
+    size_t data_len);
+
+bool ReadTlsProtobufMessageHeader(
+    TlsConnection *cxn,
+    uint8_t *out_type,
+    uint32_t *out_size);
 
 } // namespace network
 #endif // NETWORK_TLSUTILITIES_H
