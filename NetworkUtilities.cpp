4f8c5e2261151390f90c409c40ae546c6807326e
Trevor Assaf <bozkurtus@google.com>
2020-02-11 09:48:30 -0800

    Fix SetNonBlocking bug

--- NetworkUtilities.cpp
+++ NetworkUtilities.cpp
@@ -8,7 +8,7 @@ namespace network
 
 bool SetNonBlocking(int fd) {
   int flags = fcntl(fd, F_GETFL, 0);
-  return fcntl(fd, F_SETFL, flags | O_NONBLOCK) < 0;
+  return fcntl(fd, F_SETFL, flags | O_NONBLOCK) >= 0;
 }
 
 } // namespace network
